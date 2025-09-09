9a1f35917a8e673bb8885d96280dbd336ab0b3df
mgsmith <MgSmith04@outlook.com>
2025-09-09 14:21:06 +0000

    finished shell.cpp and passed test

--- lab2/shell.cpp
+++ lab2/shell.cpp
@@ -2,6 +2,8 @@
 LE2: Introduction to Unnamed Pipes
 ****************/
 #include <unistd.h> // pipe, fork, dup2, execvp, close
+#include <sys/wait.h>
+#include <iostream>   // std::cout, std::cerr
 using namespace std;
 
 int main () {
@@ -12,16 +14,49 @@ int main () {
 
     // TODO: add functionality
     // Create pipe
+    int fds[2];
+    if (pipe(fds) == -1) {
+        cerr << "pipe failed\n";
+        return 1;
+    }
 
     // Create child to run first command
-    // In child, redirect output to write end of pipe
-    // Close the read end of the pipe on the child side.
-    // In child, execute the command
-
-    // Create another child to run second command
-    // In child, redirect input to the read end of the pipe
-    // Close the write end of the pipe on the child side.
-    // Execute the second command.
-
-    // Reset the input and output file descriptors of the parent.
+    pid_t pid = fork();
+    if (pid == -1) {
+        cerr << "fork failed\n";
+        return 1;
+    }
+    if (pid == 0) {
+        // In child, redirect output to write end of pipe
+        dup2(fds[1], 1);
+        // Close the read end of the pipe on the child side.
+        close(fds[0]);
+        // In child, execute the command
+        execvp(cmd1[0], cmd1);
+        return 1;
+    }
+    else {
+        // Create another child to run second command
+        pid = fork();
+        if (pid == -1) {
+            cerr << "fork failed\n";
+            return 1;
+        }
+        if (pid == 0) {
+            // In child, redirect input to the read end of the pipe
+            // read(fds[0], stdin, sizeof(stdin));
+            dup2(fds[0], 0);
+            // Close the write end of the pipe on the child side.
+            close(fds[1]);
+            // Execute the second command.
+            execvp(cmd2[0], cmd2);
+            return 1;
+        } else {
+            // Reset the input and output file descriptors of the parent.
+            wait(nullptr);
+            close(fds[0]);
+            close(fds[1]);
+        }
+        
+    }
 }
