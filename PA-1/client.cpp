1119d844c4ac27ba91b46a389c802d980b3fed6e
mgsmith <MgSmith04@outlook.com>
2025-09-26 22:00:21 +0000

    added -m flag to support user-specified message sizes

--- PA-1/client.cpp
+++ PA-1/client.cpp
@@ -22,9 +22,10 @@ int main (int argc, char *argv[]) {
 	int p = 0; // default value to avoid unnecessary computation time
 	double t = 0.0;
 	int e = 0;
+	int m = MAX_MESSAGE; 
 	
 	string filename = "";
-	while ((opt = getopt(argc, argv, "p:t:e:f:")) != -1) {
+	while ((opt = getopt(argc, argv, "p:t:e:f:m:")) != -1) {
 		switch (opt) {
 			case 'p':
 				p = atoi (optarg); // atoi = 'alpha to int'
@@ -38,6 +39,10 @@ int main (int argc, char *argv[]) {
 			case 'f':
 				filename = optarg;
 				break;
+			case 'm':
+				m = atoi (optarg);
+				break;
+
 		}
 	}
 
@@ -49,7 +54,9 @@ int main (int argc, char *argv[]) {
     }
 	if (pid == 0) {
 		// child process, run server
-		char* args[] = {(char*) "./server", (char*) nullptr};
+		char msgSizeChar[32]; // 32 char buffer to convert m from int to char*
+		sprintf(msgSizeChar, "%i", m);
+		char* args[] = {(char*) "./server", (char*) "-m", (char*) msgSizeChar, (char*) nullptr};
 		execvp(args[0], args);
 		return 1; // if server creation fails
 	}
@@ -69,7 +76,7 @@ int main (int argc, char *argv[]) {
 	// 4.2: Requesting Data Points
 	// only do it if data points were requested
 	if (p != 0 && e != 0) {
-		char buf[MAX_MESSAGE]; // 256
+		char* buf = new char[m];
 		datamsg x(p, t, e);
 		
 		memcpy(buf, &x, sizeof(datamsg));
@@ -77,11 +84,12 @@ int main (int argc, char *argv[]) {
 		double reply;
 		chan.cread(&reply, sizeof(double)); //answer
 		cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
+		delete[] buf;
 	}
 	if (p != 0 && e == 0) {
 		// get first 1000 data points for a given person and put them in a new file 
 		// TESTME
-		char buf[MAX_MESSAGE]; // 256
+		char* buf = new char[m]; 
 		datamsg y(p, t, e);
 		int newFd;
 		char* newFile[] = {(char*) "received/x1.csv", (char*) nullptr};
@@ -90,7 +98,7 @@ int main (int argc, char *argv[]) {
 			cerr << "file creation failed\n";
 			return 1;
 		}
-		char recBuf[MAX_MESSAGE]; // buffer to receive data points
+		char* recBuf = new char[m]; // buffer to receive data points
 		double ecgReply1, ecgReply2; // recieve server replies
 		t = 0.0;
 		for (int i = 0; i < 1000; i++) {
@@ -120,6 +128,8 @@ int main (int argc, char *argv[]) {
 			t = t + 0.004;
 		}
 		close(newFd);
+		delete[] buf;
+		delete[] recBuf;
 	}
 	
 	 
@@ -150,25 +160,25 @@ int main (int argc, char *argv[]) {
 		}
 		// copy requested file to the new one
 		__int64_t remainingBytes = fileSize;
-		fm.length = MAX_MESSAGE; // message length
-		char fileBuf[MAX_MESSAGE]; // buffer to receive chunks of file data
+		fm.length = m; // message length
+		char* fileBuf = new char[m]; // buffer to receive chunks of file data
 		while (remainingBytes > 255) {
 			// cout << "rBytes: " << remainingBytes; // DEBUG LINE
 			memcpy(buf2, &fm, sizeof(filemsg)); // copy fm to buf2
 			strcpy(buf2 + sizeof(filemsg), fname.c_str()); // buf2 contains fm and the name of the desired file
 			chan.cwrite(buf2, len); // send fm to server
-			chan.cread(fileBuf, MAX_MESSAGE); // receive reply and put it in a buffer
-			write(copyFD, fileBuf, MAX_MESSAGE); // write buffer to the file
+			chan.cread(fileBuf, m); // receive reply and put it in a buffer
+			write(copyFD, fileBuf, m); // write buffer to the file
 
-			remainingBytes = remainingBytes - MAX_MESSAGE; // iterate down by 256
-			fm.offset = fm.offset + MAX_MESSAGE;
+			remainingBytes = remainingBytes - m; // iterate down by m
+			fm.offset = fm.offset + m;
 			// cout << "     e      " << remainingBytes << endl; // DEBUG LINE
 		}
 
 		// last chunk of data might not be full size
 		if (remainingBytes > 0) {
 			// cout << "out rBytes: " << remainingBytes << endl; // DEBUG LINE
-			fm.length = fileSize % MAX_MESSAGE;
+			fm.length = fileSize % m;
 			// cout << "out offset: " << fm.offset << endl; // DEBUG LINE
 			memcpy(buf2, &fm, sizeof(filemsg)); // copy fm to buf2
 			strcpy(buf2 + sizeof(filemsg), fname.c_str()); // buf2 contains fm and the name of the desired file
@@ -178,10 +188,13 @@ int main (int argc, char *argv[]) {
 		}
 		
 		delete[] buf2;
+		delete[] fileBuf;
 		close(copyFD);
 	}
 
+	// 4.4: Requesting a new channel
+
 	// closing the channel & server
-    MESSAGE_TYPE m = QUIT_MSG;
-    chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+    MESSAGE_TYPE msg = QUIT_MSG;
+    chan.cwrite(&msg, sizeof(MESSAGE_TYPE));
 }
